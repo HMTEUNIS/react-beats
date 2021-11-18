8dc4fe9d77d746fb15ed735e0f685f63811b8a68
webster <wbian618@gmail.com>
2021-11-18 10:55:28 -0500

    merge

--- src/Saved.js
+++ src/Saved.js
@@ -3,7 +3,7 @@ import './Saved.css'
 
 const Saved = ({fetched, setSelected}) => {
     
-    const displayBeats = fetched.map (beat =>  <button className="span"  onClick={() => setSelected(beat.id-1)} >Beat {beat.id}</button>)
+    const displayBeats = fetched.map (beat =>  <button className="span" id={beat.id} onClick={() => setSelected(beat.id-1)} >Beat {beat.id}</button>)
 
     return (
         <div>
