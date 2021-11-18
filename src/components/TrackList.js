b54a85e24273184fe21e79dffc735a7186db7e59
Holly Teunis <+>
2021-11-18 13:13:57 -0500

    most functional version yet!

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -3,9 +3,21 @@ import { Context } from '../hooks/useStore'
 import { soundFiles } from '../constants/config'
 import Track from './Track'
 import { context } from 'tone'
+
 const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
     const { sequence: { trackList, noteCount } } = useContext(Context)
   
+    const [soundFileSelection,setSoundFileSelection] = useState({
+        'Kick': trackList[0].soundFile,
+        'Snare': trackList[1].soundFile,
+        'Open Hat': trackList[2].soundFile,
+        'Closed Hat': trackList[3].soundFile,
+        // 'cymbal': trackList[4].soundFile
+    })
+    const handleSoundSelection = (e) => {
+        setSoundFileSelection({...soundFileSelection, [e.target.id]:e.target.value})
+    }
+
     let toSave = trackList.map( track => {
        
         const soundSave = {
@@ -18,6 +30,7 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
     console.log('re',re)
     function saveIt (e) {
           
+
         let newBeat ={ 
         trackListInfo : [
             {
@@ -39,7 +52,9 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
            
         ]  }
         
+
    console.log(newBeat)
+
           fetch(`http://localhost:4000/beats/${selected+1}`, {
   method: 'PATCH', 
   headers: {
@@ -55,8 +70,10 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
   console.error('Error:', error);
 });
     }
+
     function del (e) {
           
+
         let newBeat ={ 
         trackListInfo : [
             {
@@ -78,7 +95,9 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
            
         ]  }
         
+
    console.log(newBeat)
+
           fetch(`http://localhost:4000/beats/${selected+1}`, {
   method: 'PATCH', 
   headers: {
@@ -97,10 +116,19 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
     
     
 const gettin = fetched[selected]
+
 let loadKicks = gettin.trackListInfo[0].notes
 let loadSnares = gettin.trackListInfo[1].notes
 let loadHho = gettin.trackListInfo[2].notes
 let loadHhc = gettin.trackListInfo[3].notes
+
+const savedTrackSounds = {
+    kickTrack : gettin.trackListInfo[0].name,
+    snareTrack : gettin.trackListInfo[1].name,
+    ohhTrack : gettin.trackListInfo[2].name,
+    chhTrack : gettin.trackListInfo[3].name
+}
+
 function loadin (e){
     trackList[0].onNotes = []
     trackList[1].onNotes = []
@@ -110,13 +138,26 @@ function loadin (e){
     trackList[1].onNotes = loadSnares
     trackList[2].onNotes = loadHho
     trackList[3].onNotes = loadHhc
+
+    trackList[0].name = savedTrackSounds.kickTrack
+    trackList[1].name = savedTrackSounds.snareTrack
+    trackList[2].name = savedTrackSounds.ohhTrack
+    trackList[3].name = savedTrackSounds.chhTrack
 }
+
+    const tempTrackList = [...trackList]
+        tempTrackList.map((track)=>{
+            const title = track.title
+            track.soundFile = soundFileSelection[title]
+        })
+
     let content = trackList.map((track, trackID) => {
         let key = trackID
         let { title, onNotes, soundFile, id } = track
         let soundFilePath = soundFiles[soundFile]
         return (
         <> 
+
             <Track
             
                 id = {id}
@@ -127,6 +168,7 @@ function loadin (e){
                 noteCount={noteCount}
                 onNotes={onNotes}
                 soundFilePath={soundFilePath}
+                handleSoundSelection={handleSoundSelection}
             />
             </>
         )
