af20b324e1c7abe05443733f181fc510c92a5eb9
Holly Teunis <+>
2021-11-18 18:22:08 -0500

    with skeleton

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css"
 import {Link} from "react-router-dom"
+// import skel from "skel.gif"
 
 const Home = () => {
     return (
@@ -10,7 +11,8 @@ const Home = () => {
             <Link to= '/app'> <span className="span"><button className="hsbutt">Application</button></span></ Link>
             <br />
             <p2>Special Thanks to Joe Seifi and Ganesh Mani. We've never met, but your work was so influencial and helpful in ours we couldn't not name you. </p2>
-            
+            <br />
+            <img src="https://i.pinimg.com/originals/0e/4d/60/0e4d6062d1bd7d7a077739f68be05ccd.gif" />
         </div>
     )
 }
