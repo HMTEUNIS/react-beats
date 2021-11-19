d5c657fbacd6d142dc4df41cc13a5e535482ca41
Daniel <dandesantis14@gmail.com>
2021-11-18 19:20:17 -0500

    track selection and dynamic track load working

--- src/components/Track.js
+++ src/components/Track.js
@@ -9,6 +9,7 @@ let Track = ({
     title,
     noteCount,
     onNotes,
+    soundFile,
     soundFilePath,
     handleSoundSelection
 }) => {
@@ -31,64 +32,67 @@ let Track = ({
         )
     })
 
+    const selectedTrackSound = soundFile
+    console.log(selectedTrackSound)
+    
     let trackOptions;
     
     switch(title){
         case "Kick":
             trackOptions =
-                (<select className="track_title" id='Kick' onChange={handleSoundSelection}>
-                    <option value="kick1">Kick1</option>
-                    <option value="kick2">Kick2</option>
-                    <option value="kick3">Kick3</option>
-                    <option value="kick4">Kick4</option>
-                    <option value="kick5">Kick5</option>
-                    <option value="kick6">Kick6</option>
+                (<select className="track-select" id='Kick' onChange={handleSoundSelection} value={selectedTrackSound}>
+                    <option value="kick1">Kick 1</option>
+                    <option value="kick2">Kick 2</option>
+                    <option value="kick3">Kick 3</option>
+                    <option value="kick4">Kick 4</option>
+                    <option value="kick5">Kick 5</option>
+                    <option value="kick6">Kick 6</option>
                 </select>)
             break;
         case "Snare":
             trackOptions = 
-                (<select className="track_title" id='Snare' onChange={handleSoundSelection}>
-                    <option value="snare1">Snare1</option>
-                    <option value="snare2">Snare2</option>
-                    <option value="snare3">Snare3</option>
-                    <option value="snare4">Snare4</option>
-                    <option value="snare5">Snare5</option>
-                    <option value="snare6">Snare6</option>
+                (<select className="track-select" id='Snare' onChange={handleSoundSelection} value={selectedTrackSound}>
+                    <option value="snare1">Snare 1</option>
+                    <option value="snare2">Snare 2</option>
+                    <option value="snare3">Snare 3</option>
+                    <option value="snare4">Snare 4</option>
+                    <option value="snare5">Snare 5</option>
+                    <option value="snare6">Snare 6</option>
                 </select>)
             break;
         case "Open Hat":
             trackOptions = 
-                (<select className="track_title" id='Open Hat' onChange={handleSoundSelection}>
-                    <option value="hh_open1">Open Hat1</option>
-                    <option value="hh_open2">Open Hat2</option>
-                    <option value="hh_open3">Open Hat3</option>
-                    <option value="hh_open4">Open Hat4</option>
-                    <option value="hh_open5">Open Hat5</option>
-                    <option value="hh_open6">Open Hat6</option>
+                (<select className="track-select" id='Open Hat' onChange={handleSoundSelection} value={selectedTrackSound}>
+                    <option value="hh_open1">Open Hat 1</option>
+                    <option value="hh_open2">Open Hat 2</option>
+                    <option value="hh_open3">Open Hat 3</option>
+                    <option value="hh_open4">Open Hat 4</option>
+                    <option value="hh_open5">Open Hat 5</option>
+                    <option value="hh_open6">Open Hat 6</option>
                 </select>)
             break;
         case "Closed Hat":
             trackOptions = 
-                (<select className="track_title" id='Closed Hat' onChange={handleSoundSelection}>
-                    <option value="hh_closed1">Closed Hat1</option>
-                    <option value="hh_closed2">Closed Hat2</option>
-                    <option value="hh_closed3">Closed Hat3</option>
-                    <option value="hh_closed4">Closed Hat4</option>
-                    <option value="hh_closed5">Closed Hat5</option>
-                    <option value="hh_closed6">Closed Hat6</option>
+                (<select className="track-select" id='Closed Hat' onChange={handleSoundSelection} value={selectedTrackSound}>
+                    <option value="hh_closed1">Closed Hat 1</option>
+                    <option value="hh_closed2">Closed Hat 2</option>
+                    <option value="hh_closed3">Closed Hat 3</option>
+                    <option value="hh_closed4">Closed Hat 4</option>
+                    <option value="hh_closed5">Closed Hat 5</option>
+                    <option value="hh_closed6">Closed Hat 6</option>
                 </select>)
             break;
-        case "Cymbal":
-            trackOptions = 
-                (<select className="track_title" id='Cymbal' onChange={handleSoundSelection}>
-                    <option value="cymbal1">Cymbal1</option>
-                    <option value="cymbal2">Cymbal2</option>
-                    <option value="cymbal3">Cymbal3</option>
-                    <option value="cymbal4">Cymbal4</option>
-                    <option value="cymbal5">Cymbal5</option>
-                    <option value="cymbal6">Cymbal6</option>
-                </select>)
-            break;
+        // case "Cymbal":
+        //     trackOptions = 
+        //         (<select className="track-select" id='Cymbal' onChange={handleSoundSelection}>
+        //             <option value="cymbal1">Cymbal1</option>
+        //             <option value="cymbal2">Cymbal2</option>
+        //             <option value="cymbal3">Cymbal3</option>
+        //             <option value="cymbal4">Cymbal4</option>
+        //             <option value="cymbal5">Cymbal5</option>
+        //             <option value="cymbal6">Cymbal6</option>
+        //         </select>)
+        //     break;
         }
     
     return (
--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -6,8 +6,8 @@ import { context } from 'tone'
 
 const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
     const { sequence: { trackList, noteCount } } = useContext(Context)
-  
-    const [soundFileSelection,setSoundFileSelection] = useState({
+
+    const [soundFileSelection, setSoundFileSelection] = useState({
         'Kick': trackList[0].soundFile,
         'Snare': trackList[1].soundFile,
         'Open Hat': trackList[2].soundFile,
@@ -15,179 +15,209 @@ const TrackList = ({ currentStepID, selected, fetched, re, setre }) => {
         // 'cymbal': trackList[4].soundFile
     })
     const handleSoundSelection = (e) => {
-        setSoundFileSelection({...soundFileSelection, [e.target.id]:e.target.value})
+        setSoundFileSelection({ ...soundFileSelection, [e.target.id]: e.target.value })
     }
 
-    let toSave = trackList.map( track => {
-       
-        const soundSave = {
-            name : track.soundFile,
-            notes: track.onNotes
-        } 
-    return    soundSave
-    })
+    // let toSave = trackList.map(track => {
+
+    //     const soundSave = {
+    //         name: track.soundFile,
+    //         notes: track.onNotes
+    //     }
+    //     return soundSave
+    // })
     console.log(selected)
-    console.log('re',re)
-    function saveIt (e) {
-          
+    console.log('re', re)
 
-        let newBeat ={ 
-        trackListInfo : [
-            {
-                "name": toSave[0].name,
-                "notes": toSave[0].notes
-            },
-            {
-                "name": toSave[1].name,
-                "notes": toSave[1].notes
-            },
-            {
-                "name": toSave[2].name,
-                "notes": toSave[2].notes
-            },
-            {
-                "name": toSave[3].name,
-                "notes": toSave[3].notes
+    function saveIt(e) {
+        let toSave = trackList.map(track => {
+
+            const soundSave = {
+                name: track.soundFile,
+                notes: track.onNotes
             }
-           
-        ]  }
-        
-
-   console.log(newBeat)
-
-          fetch(`http://localhost:4000/beats/${selected+1}`, {
-  method: 'PATCH', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(newBeat),
-})
-.then(res => res.json())
-.then(data => {
-  setre(!re);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-    }
-
-    function del (e) {
-          
-
-        let newBeat ={ 
-        trackListInfo : [
-            {
-                "name": toSave[0].name,
-                "notes": []
-            },
-            {
-                "name": toSave[1].name,
-                "notes": []
-            },
-            {
-                "name": toSave[2].name,
-                "notes": []
-            },
-            {
-                "name": toSave[3].name,
-                "notes": []
-            }
-           
-        ]  }
-        
-
-   console.log(newBeat)
-
-          fetch(`http://localhost:4000/beats/${selected+1}`, {
-  method: 'PATCH', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(newBeat),
-})
-.then(res => res.json())
-.then(data => {
-  setre(!re);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-    }
-    
-    
-const gettin = fetched[selected]
-
-let loadKicks = gettin.trackListInfo[0].notes
-let loadSnares = gettin.trackListInfo[1].notes
-let loadHho = gettin.trackListInfo[2].notes
-let loadHhc = gettin.trackListInfo[3].notes
-
-const savedTrackSounds = {
-    kickTrack : gettin.trackListInfo[0].name,
-    snareTrack : gettin.trackListInfo[1].name,
-    ohhTrack : gettin.trackListInfo[2].name,
-    chhTrack : gettin.trackListInfo[3].name
-}
-
-function loadin (e){
-    trackList[0].onNotes = []
-    trackList[1].onNotes = []
-    trackList[2].onNotes = []
-    trackList[3].onNotes = []
-    trackList[0].onNotes = loadKicks
-    trackList[1].onNotes = loadSnares
-    trackList[2].onNotes = loadHho
-    trackList[3].onNotes = loadHhc
-
-    trackList[0].name = savedTrackSounds.kickTrack
-    trackList[1].name = savedTrackSounds.snareTrack
-    trackList[2].name = savedTrackSounds.ohhTrack
-    trackList[3].name = savedTrackSounds.chhTrack
-}
-
-    const tempTrackList = [...trackList]
-        tempTrackList.map((track)=>{
-            const title = track.title
-            track.soundFile = soundFileSelection[title]
+            return soundSave
         })
 
-    let content = trackList.map((track, trackID) => {
+        let newBeat = {
+            trackListInfo: [
+                {
+                    "name": toSave[0].name,
+                    "notes": toSave[0].notes
+                },
+                {
+                    "name": toSave[1].name,
+                    "notes": toSave[1].notes
+                },
+                {
+                    "name": toSave[2].name,
+                    "notes": toSave[2].notes
+                },
+                {
+                    "name": toSave[3].name,
+                    "notes": toSave[3].notes
+                }
+
+            ]
+        }
+
+
+        console.log(newBeat)
+
+        fetch(`http://localhost:4000/beats/${selected + 1}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newBeat),
+        })
+            .then(res => res.json())
+            .then(data => {
+                setre(!re);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    function del(e) {
+        let toSave = trackList.map(track => {
+
+            const soundSave = {
+                name: track.soundFile,
+                notes: track.onNotes
+            }
+            return soundSave
+        })
+
+        let newBeat = {
+            trackListInfo: [
+                {
+                    "name": toSave[0].name,
+                    "notes": []
+                },
+                {
+                    "name": toSave[1].name,
+                    "notes": []
+                },
+                {
+                    "name": toSave[2].name,
+                    "notes": []
+                },
+                {
+                    "name": toSave[3].name,
+                    "notes": []
+                }
+
+            ]
+        }
+
+
+        console.log(newBeat)
+
+        fetch(`http://localhost:4000/beats/${selected + 1}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newBeat),
+        })
+            .then(res => res.json())
+            .then(data => {
+                setre(!re);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+
+    const gettin = fetched[selected]
+
+    let loadKicks = gettin.trackListInfo[0].notes
+    let loadSnares = gettin.trackListInfo[1].notes
+    let loadHho = gettin.trackListInfo[2].notes
+    let loadHhc = gettin.trackListInfo[3].notes
+
+    // const savedTrackSounds = {
+    //     kickTrack : gettin.trackListInfo[0].name,
+    //     snareTrack : gettin.trackListInfo[1].name,
+    //     ohhTrack : gettin.trackListInfo[2].name,
+    //     chhTrack : gettin.trackListInfo[3].name
+    // }
+
+    function loadin(e) {
+        trackList[0].onNotes = []
+        trackList[1].onNotes = []
+        trackList[2].onNotes = []
+        trackList[3].onNotes = []
+        trackList[0].onNotes = loadKicks
+        trackList[1].onNotes = loadSnares
+        trackList[2].onNotes = loadHho
+        trackList[3].onNotes = loadHhc
+
+        const savedTrackSounds = {
+            kickTrack: gettin.trackListInfo[0].name,
+            snareTrack: gettin.trackListInfo[1].name,
+            ohhTrack: gettin.trackListInfo[2].name,
+            chhTrack: gettin.trackListInfo[3].name
+        }
+
+        trackList[0].name = savedTrackSounds.kickTrack
+        trackList[1].name = savedTrackSounds.snareTrack
+        trackList[2].name = savedTrackSounds.ohhTrack
+        trackList[3].name = savedTrackSounds.chhTrack
+
+        setSoundFileSelection({
+            'Kick': trackList[0].name,
+            'Snare': trackList[1].name,
+            'Open Hat': trackList[2].name,
+            'Closed Hat': trackList[3].name,
+            // 'cymbal': trackList[4].name
+        })
+    }
+    console.log(trackList)
+    const tempTrackList = trackList.map((track) => {
+        const title = track.title
+        track.soundFile = soundFileSelection[title]
+        console.log(track)
+        return track
+    })
+
+    let content = tempTrackList.map((track, trackID) => {
         let key = trackID
         let { title, onNotes, soundFile, id } = track
         let soundFilePath = soundFiles[soundFile]
         return (
-        <> 
+            <>
 
-            <Track
-            
-                id = {id}
-                key={title}
-                trackID={+trackID}
-                currentStepID={currentStepID}
-                title={title}
-                noteCount={noteCount}
-                onNotes={onNotes}
-                soundFilePath={soundFilePath}
-                handleSoundSelection={handleSoundSelection}
-            />
+                <Track
+
+                    id={id}
+                    key={title}
+                    trackID={+trackID}
+                    currentStepID={currentStepID}
+                    title={title}
+                    noteCount={noteCount}
+                    onNotes={onNotes}
+                    soundFile={soundFile}
+                    soundFilePath={soundFilePath}
+                    handleSoundSelection={handleSoundSelection}
+                />
             </>
         )
     })
-    
+
     return (
         <>
-        
-        <div className="track-list">
-        
-            {content}
-        </div>
-        <button className="tlButtons" onClick={(e) => saveIt()} > SAVE TO SELECTED </ button>
-        <button className="tlButtons"  onClick={(e) => loadin()} > LOAD SELECTED BEAT </ button>
-        <br />
+            <div className="track-list">
+                {content}
+            </div>
+            <button className="tlButtons" onClick={(e) => saveIt()} > SAVE TO SELECTED </ button>
+            <button className="tlButtons" onClick={(e) => loadin()} > LOAD SELECTED BEAT </ button>
+            <br />
             <button className="tlButtons" onClick={(e) => del()}>CLEAR SELECTED</button>
-        <p1> Beat {selected} Selected!</p1>
-       
-       
+            <p1> Beat {selected} Selected!</p1>
         </>
     )
 }
